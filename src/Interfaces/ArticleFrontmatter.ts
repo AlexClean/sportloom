abb358c6ed76120b27df73e6c99a286cee29fbc6
Samokhval Alexey <samohalexey@gmail.com>
2025-09-15 11:00:52 +0200

    added .mdx file and compileMdx

---
+++ src/Interfaces/ArticleFrontmatter.ts
@@ -0,0 +1,9 @@
+export interface ArticleFrontmatter {
+    title: string
+    description: string
+    date: Date
+    slug: string
+    category?: string
+    cover?: string
+    readingTime?: string
+}
