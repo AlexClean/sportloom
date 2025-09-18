f58b2ee5a283471827e0825c9d456f9ff02a5642
Samokhval Alexey <samohalexey@gmail.com>
2025-09-18 12:18:18 +0200

    SLP-60 added favicons and Metainformation for it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ import Footer from "@/app/components/footer/Footer";
 export const metadata: Metadata = {
   title: "Sportloom — Your Source for Sports Gear & News",
   description: "Latest sport news and overviews about sport equipments",
+  manifest:"/manifest.json",
+  icons: {
+    icon: [
+    {url:"/favicon-16x16.png", sizes:"16x16", type:"image/png"},
+    {url:"/favicon-32x32.png", sizes:"32x32", type:"image/png"},
+    {url:"/favicon-48x48.png", sizes:"48x48", type:"image/png"},
+    {url:"/favicon-180x180.png", sizes:"180x180", type:"image/png"},
+    {url:"/favicon.ico", sizes:"any"}
+  ],
+  apple: [{url:"/apple-touch-icon.png", sizes:"180x180"}],
+  }
 };
 
 export default function RootLayout({
