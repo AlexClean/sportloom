e6c00b8f75fd4d7e66e5278a48df4f3544272b00
Samokhval Alexey <samohalexey@gmail.com>
2025-10-15 15:54:31 +0200

    added mdx file with the article

--- src/app/components/mdx/icons.ts
+++ src/app/components/mdx/icons.ts
@@ -1,9 +1,12 @@
-import { CircleCheck, HandCoins, ShieldCheck, Zap, BadgeDollarSign } from "lucide-react";
+import { CircleCheck, HandCoins, ShieldCheck, Zap, BadgeDollarSign, Leaf, Award, Target   } from "lucide-react";
 
 export const verdictIcons = {
     circle: CircleCheck,
     coins: HandCoins,
     shield: ShieldCheck,
     zap: Zap,
-    badgeDollar: BadgeDollarSign
+    badgeDollar: BadgeDollarSign,
+    leaf: Leaf,
+    award: Award,
+    target: Target
 }
