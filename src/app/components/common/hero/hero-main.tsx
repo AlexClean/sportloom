edd19e63ccb7cae382591d30df7f8ff095dbb842
Samokhval Alexey <samohalexey@gmail.com>
2026-03-28 18:50:16 +0100

    Logo fix

--- src/app/components/common/hero/hero-main.tsx
+++ src/app/components/common/hero/hero-main.tsx
@@ -17,7 +17,7 @@ export default function HeroMain({
             <h1 className="text-6xl font-bold mb-4">Welcome to Sportloom</h1>
             <Image src={src} alt={alt} className={className || "mx-auto mt-8 mb-6 rounded-lg shadow-lg"} width={1800} height={1400} />
             <h2 className="text-4xl text-gray-900 mx-auto dark:text-gray-200">
-                Sportlum: Your Unbiased Guide to the World of Sport. Expert Reviews, In-Depth Articles, and Breaking News Soon.
+                Sportloom: Your Unbiased Guide to the World of Sport. Expert Reviews, In-Depth Articles, and Breaking News Soon.
             </h2>
         </section>
     );
