f62a521810060a7d66555b9a2c33c3ade807176b
Samokhval Alexey <samohalexey@gmail.com>
2026-03-30 16:05:29 +0200

    added Google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/app/components/header/Header";
 import Footer from "@/app/components/footer/Footer";
-import {Inter, Oswald} from "next/font/google";
+import { Inter, Oswald } from "next/font/google";
 
-const inter = Inter({ 
+const inter = Inter({
   subsets: ['latin', 'cyrillic'],
   variable: '--font-inter',
   display: 'swap', // Важно для производительности
@@ -20,16 +21,16 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://sportloom.com'),
   title: "Sportloom — Your Source for Sports Gear & News",
   description: "Latest sport news and overviews about sport equipments",
-  manifest:"/manifest.json",
+  manifest: "/manifest.json",
   icons: {
     icon: [
-    {url:"/favicon-16x16.png", sizes:"16x16", type:"image/png"},
-    {url:"/favicon-32x32.png", sizes:"32x32", type:"image/png"},
-    {url:"/favicon-48x48.png", sizes:"48x48", type:"image/png"},
-    {url:"/favicon-180x180.png", sizes:"180x180", type:"image/png"},
-    {url:"/favicon.ico", sizes:"any"}
-  ],
-  apple: [{url:"/apple-touch-icon.png", sizes:"180x180"}],
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48x48.png", sizes: "48x48", type: "image/png" },
+      { url: "/favicon-180x180.png", sizes: "180x180", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" }
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   }
 };
 
@@ -40,10 +41,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${oswald.variable}`}>
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-FC0DD7LVEM" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-FC0DD7LVEM');`}
+        </Script>
+      </head>
       <body className='app-shell bg-white text-gray-900'>
-        <Header/>
+        <Header />
         <main className='app-content min-h-screen container mx-auto px-4 py-6 pt-16'>{children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
