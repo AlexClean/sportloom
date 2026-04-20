52c9d2523b22805339a15f252359cdc1aeb985c4
Samokhval Alexey <samohalexey@gmail.com>
2026-04-20 17:53:13 +0200

    added ArticleHeader component and updated meta.json files

--- src/app/components/header/ArticleHeader.tsx
+++ src/app/components/header/ArticleHeader.tsx
@@ -13,17 +13,18 @@ export default function ArticleHeader({ meta }: ArticleHeaderProps) {
     const heroAlt = altText || title;
 
     return (
-        <>
-            <header className="mb-8">
-                <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight">{title}</h1>
-                <p className="mt-2 text-base text-neutral-600 dark:text-neutral-400">{subtitle}</p>
-                <div className="mt-3 text-sm text-neutral-500"><time dateTime={date}>{date}</time></div>
-            </header>
+
+        <header className="mb-8">
+            <h1 className="text-3xl text-center md:text-4xl font-extrabold tracking-tight">{title}</h1>
+            <p className="mt-2 text-base text-neutral-600 dark:text-neutral-400">{subtitle}</p>
+            <div className="my-3 text-sm text-neutral-500">
+                <time dateTime={date}>{setDatetimeAttribute(date)}</time>
+            </div>
             <figure className="mb-8">
                 <Image
                     src={heroSrc}
                     alt={heroAlt}
-                    width={120}
+                    width={920}
                     height={613}
                     className="w-full rounded-xl object-cover"
                     loading="eager"
@@ -31,6 +32,20 @@ export default function ArticleHeader({ meta }: ArticleHeaderProps) {
                 />
                 <figcaption className="mt-2 text-xs text-neutral-500">Photo: Sportloom</figcaption>
             </figure>
-        </>
+        </header>
     )
+}
+
+
+const setDatetimeAttribute = (dateString: string | undefined): string => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    } as const;
+
+    return date.toLocaleDateString('us-US', options)
 }
