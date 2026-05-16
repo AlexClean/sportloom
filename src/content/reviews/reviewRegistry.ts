15f921a6d34dc8f7f028f623dcc5d034a1565234
AlexClean <samohalexey@gmail.com>
2026-05-16 16:03:12 +0200

    deleted Best boxing gloves for heavy bag 2025 review

--- src/content/reviews/reviewRegistry.ts
+++ src/content/reviews/reviewRegistry.ts
@@ -1,5 +1,4 @@
 import { reviewData as bestBoxingGlovesForBeginners2025 } from "@/content/reviews/best-boxing-gloves-for-beginners-2025/data";
-import { reviewData as bestBoxingGlovesForHeavyBag2025 } from "@/content/reviews/best-boxing-gloves-for-heavy-bag-2025/data";
 import { reviewData as bestBoxingGlovesForSparring2026 } from "@/content/reviews/best-boxing-gloves-for-sparring-2026/data";
 import {reviewData as bestBoxingGloves2025 } from "@/content/reviews/best-boxing-gloves-2025/data";
 import { reviewData as bestBoxingUnder100 } from "./best-boxing-gloves-under-100/data";
@@ -14,7 +13,6 @@ export const REVIEW_DATA_INDEX: Record<string, ReviewPageData> =
     {
         [bestBoxingGloves2025.slug]: bestBoxingGloves2025,
         [bestBoxingGlovesForBeginners2025.slug]: bestBoxingGlovesForBeginners2025,
-        [bestBoxingGlovesForHeavyBag2025.slug]: bestBoxingGlovesForHeavyBag2025,
         [bestBoxingGlovesForSparring2026.slug]: bestBoxingGlovesForSparring2026,
         [bestBoxingGlovesForTraining.slug]: bestBoxingGlovesForTraining,
         [bestBoxingUnder100.slug]: bestBoxingUnder100,
